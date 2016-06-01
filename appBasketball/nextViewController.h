7fb5508fc834c98195edb55c921b7cd96be569a8
Elisha Marshall <iamelimars@yahoo.com>
2016-06-01 11:31:00 -0400

    receive and update text messages

--- appBasketball/nextViewController.h
+++ appBasketball/nextViewController.h
@@ -7,8 +7,21 @@
 //
 
 #import "ViewController.h"
+#import "AppState.h"
+#import "Constants.h"
 
-@interface nextViewController : ViewController
+@import Photos;
+
+@import Firebase;
+@interface nextViewController : ViewController <UITableViewDataSource, UITableViewDelegate,
+UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate> {
+    int _msglength;
+}
+
 - (IBAction)signOutButton:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *sendButton;
+@property (weak, nonatomic) IBOutlet UITableView *clientTable;
+- (IBAction)didSendMessage:(id)sender;
 
 @end
--- appBasketball/nextViewController.m
+++ appBasketball/nextViewController.m
@@ -12,8 +12,18 @@
 #import "Constants.h"
 #import "MeasurementHelper.h"
 @import Firebase;
+@import FirebaseDatabase;
 
-@interface nextViewController ()
+@interface nextViewController (){
+    
+    FIRDatabaseHandle _refHandle;
+}
+
+
+@property (strong, nonatomic) FIRDatabaseReference *ref;
+@property (strong, nonatomic) NSMutableArray<FIRDataSnapshot *> *messages;
+//@property (strong, nonatomic) FIRStorageReference *storageRef;
+//@property (nonatomic, strong) FIRRemoteConfig *remoteConfig;
 
 @end
 
@@ -21,13 +31,90 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _clientTable.delegate = self;
+    _clientTable.dataSource = self;
+    
+    _ref = [[FIRDatabase database]reference];
     // Do any additional setup after loading the view.
+    _msglength = 10;
+    _messages = [[NSMutableArray alloc] init];
+    [_clientTable registerClass:UITableViewCell.self forCellReuseIdentifier:@"tableViewCell"];
+    
+    [self configureDatabase];
+    [self configureStorage];
+    [self configureRemoteConfig];
+    [self fetchConfig];
+    [self loadAd];
+    [self logViewLoaded];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)dealloc {
+}
+
+- (void)configureDatabase {
+}
+
+- (void)configureStorage {
+}
+
+- (void)configureRemoteConfig {
+}
+
+- (void)fetchConfig {
+}
+- (void)logViewLoaded {
+}
+
+- (void)loadAd {
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(nonnull NSString *)string {
+    NSString *text = textField.text;
+    if (!text) {
+        return YES;
+    }
+    long newLength = text.length + string.length - range.length;
+    return (newLength <= _msglength);
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [_messages removeAllObjects];
+    _refHandle = [[_ref child:@"messages"]observeEventType:FIRDataEventTypeChildAdded withBlock:^(FIRDataSnapshot *snapshot) {
+        [_messages addObject:snapshot];
+        [_clientTable insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:_messages.count-1 inSection:0]] withRowAnimation: UITableViewRowAnimationAutomatic];
+    }];
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    
+    [_ref removeObserverWithHandle:_refHandle];
+
+}
+
+
+// UITextViewDelegate protocol methods
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self sendMessage:@{MessageFieldstext: textField.text}];
+    textField.text = @"";
+    return YES;
+}
+
+- (void)sendMessage:(NSDictionary *)data {
+    NSMutableDictionary *mdata = [data mutableCopy];
+    mdata[MessageFieldsname] = [AppState sharedInstance].displayName;
+    NSURL *photoUrl = AppState.sharedInstance.photoUrl;
+    if (photoUrl) {
+        mdata[MessageFieldsphotoUrl] = [photoUrl absoluteString];
+    }
+}
+
 
 /*
 #pragma mark - Navigation
@@ -38,6 +125,35 @@
     // Pass the selected object to the new view controller.
 }
 */
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [_messages count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    // Dequeue cell
+    UITableViewCell *cell = [_clientTable dequeueReusableCellWithIdentifier:@"tableViewCell" forIndexPath:indexPath];
+    
+    // Unpack message from Firebase DataSnapshot
+    FIRDataSnapshot *messageSnapshot = _messages[indexPath.row];
+    NSDictionary<NSString *, NSString *> *message = messageSnapshot.value;
+    NSString *name = message[MessageFieldsname];
+    NSString *text = message[MessageFieldstext];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@: %@", name, text];
+    //cell.imageView.image = [UIImage imageNamed: @"ic_account_circle"];
+    NSString *photoUrl = message[MessageFieldsphotoUrl];
+    if (photoUrl) {
+        NSURL *url = [NSURL URLWithString:photoUrl];
+        if (url) {
+            NSData *data = [NSData dataWithContentsOfURL:url];
+            if (data) {
+                cell.imageView.image = [UIImage imageWithData:data];
+            }
+        }
+    }
+    return cell;
+}
+
+
 
 - (IBAction)signOutButton:(id)sender {
     
@@ -52,4 +168,19 @@
     [self performSegueWithIdentifier:SeguesFpToSignIn sender:nil];
     
 }
+- (IBAction)didSendMessage:(id)sender {
+    
+    [self textFieldShouldReturn:_textField];
+
+}
+
+- (void)showAlert:(NSString *)title message:(NSString *)message {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *dismissAction = [UIAlertAction actionWithTitle:@"Dismiss" style:UIAlertActionStyleDestructive handler:nil];
+        [alert addAction:dismissAction];
+        [self presentViewController:alert animated: true completion: nil];
+    });
+}
+
 @end
